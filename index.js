50f61f9566078fdaec92569d89a46d686a6c5ff7
Zoe Guidry <oyezoey@gmail.com>
2022-10-13 14:57:31 -0500

    commit tree 6c0d1d21dd1236d833c4399e4b5e0aa9a420c377

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor( Math.random() * 100 ) + 1;
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+const max = Math.max( 5, 11, 15, 20 );
